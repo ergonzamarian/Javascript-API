115ba8c19965cfc20d9d7bb5f95ee1b135b303a6
ergonzamarian <ergonzamarian@gmail.com>
2022-08-10 22:45:41 -0400

    Adicionando projeto concluído - Dados do CEP preenchidos com valores da API

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -5,6 +5,8 @@
 // that.variavel = 0;  ...
 
 async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = ""
     try{
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var consultaCEPConvertida = await consultaCEP.json();
@@ -12,20 +14,37 @@ async function buscaEndereco(cep) {
         if(consultaCEPConvertida.erro){
             throw Error('CEP inexistente!');
         }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var bairro = document.getElementById('bairro');
+        var estado = document.getElementById('estado');
 
+        // mudando os valores dos elementos do html com os valores de retorno do json
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        bairro.value = consultaCEPConvertida.bairro;
+        estado.value = consultaCEPConvertida.uf;
+
+        console.log(consultaCEPConvertida)
         return consultaCEPConvertida;
 
     }catch(erro){
+        mensagemErro.innerHTML = `<p>O CEP informado não existe!</p>`
         console.log(erro)
     }
 
 }
 
-let ceps = ['79117013', '79116570', '01001000', '01001001']
+var cep = document.getElementById('cep');
+// focusout = se o usuário clicar no campo e depois em qualquer lugar da tela
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
 
+
+
+// let ceps = ['01001000', '01001001']
 // criando um novo array com o que retornar 
 // da função buscaEndereco para cada um dos valores dentro do cep
 // e esses valores são promessas que precisamos resolver com Promise.all
-let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
-console.log(conjuntoCeps)
-Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
+// let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+// console.log(conjuntoCeps)
+// Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
